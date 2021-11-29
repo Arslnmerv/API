577b3b1e7bdd5dbaef5e5b5790d4a2da1da313cf
Arslnmrv <arslanmerve1619@gmail.com>
2021-11-29 21:24:26 +0300

    api/8

--- src/test/java/com/techproed/pojos/BookingPojo.java
+++ src/test/java/com/techproed/pojos/BookingPojo.java
@@ -1,5 +1,7 @@
 package com.techproed.pojos;
+
 import java.util.Map;
+
 public class BookingPojo {
     /*
     {
@@ -18,39 +20,51 @@ public class BookingPojo {
     private int totalprice;
     private boolean depositpaid;
     private BookingDatesPojo bookingdates;   //Daha önce oluşturduğum pojo kalıbını datatype olarak belirterek
+
     // yeni pojonun içerine gömmüş oldum.
     public String getFirstname() {
         return firstname;
     }
+
     public void setFirstname(String firstname) {
         this.firstname = firstname;
     }
+
     public String getLastname() {
         return lastname;
     }
+
     public void setLastname(String lastname) {
         this.lastname = lastname;
     }
+
     public int getTotalprice() {
         return totalprice;
     }
+
     public void setTotalprice(int totalprice) {
         this.totalprice = totalprice;
     }
+
     public boolean isDepositpaid() {
         return depositpaid;
     }
+
     public void setDepositpaid(boolean depositpaid) {
         this.depositpaid = depositpaid;
     }
+
     public BookingDatesPojo getBookingdates() {
         return bookingdates;
     }
+
     public void setBookingdates(BookingDatesPojo bookingdates) {
         this.bookingdates = bookingdates;
     }
+
     public BookingPojo() {
     }
+
     public BookingPojo(String firstname, String lastname, int totalprice, boolean depositpaid, BookingDatesPojo bookingdates) {
         this.firstname = firstname;
         this.lastname = lastname;
@@ -58,6 +72,7 @@ public class BookingPojo {
         this.depositpaid = depositpaid;
         this.bookingdates = bookingdates;
     }
+
     @Override
     public String toString() {
         return "BookingPojo{" +
