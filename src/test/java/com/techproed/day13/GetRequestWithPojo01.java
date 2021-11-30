7a1f632cbee33d54e126e295dbe83b01f28b0a1b
Arslnmrv <arslanmerve1619@gmail.com>
2021-12-01 00:37:35 +0300

    api/fnl

--- src/test/java/com/techproed/day13/GetRequestWithPojo01.java
+++ src/test/java/com/techproed/day13/GetRequestWithPojo01.java
@@ -1,13 +1,16 @@
 package com.techproed.day13;
 
+import com.google.gson.Gson;
 import com.techproed.pojos.Data;
 import com.techproed.pojos.DummyPojo;
 import com.techproed.testBase.DummyTestBase;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.Assert.*;
 
 public class GetRequestWithPojo01 extends DummyTestBase {
 
@@ -28,31 +31,51 @@ GET Request to the URL http://dummy.restapiexample.com/api/v1/employee/1
  */
 
     @Test
-    public void test(){
+    public void test() {
 
-        spec03.pathParams("parametre1","employee",
-                "parametre2",1);
+        spec03.pathParams("parametre1", "employee",
+                "parametre2", 1);
 
-        Data data=
-                new Data(1,"Tiger Nixon",320800,61,"");
+        Data data =
+                new Data(1, "Tiger Nixon", 320800, 61, "");
 
-        DummyPojo expectedData=
-                new DummyPojo("success",data,"Successfully! Record has been fetched.");
+        DummyPojo expectedData =
+                new DummyPojo("success", data, "Successfully! Record has been fetched.");
 
-        Response response=given().
+        Response response = given().
                 contentType(ContentType.JSON).
                 spec(spec03).
                 when().get("/{parametre1}/{parametre2}");
 
         response.prettyPrint();
 
-        DummyPojo actualData=response.as(DummyPojo.class);
+        DummyPojo actualData = response.as(DummyPojo.class);
         System.out.println(actualData);
 
+        assertEquals(200, response.getStatusCode());
+        assertEquals(expectedData.getStatus(), actualData.getStatus());
+        assertEquals(expectedData.getData().getemployee_name(),
+                actualData.getData().getemployee_name());
+        assertEquals(expectedData.getData().getId(),
+                actualData.getData().getId());
+        assertEquals(expectedData.getData().getemployee_age(),
+                actualData.getData().getemployee_age());
+        assertEquals(expectedData.getData().getemployee_salary(),
+                actualData.getData().getemployee_salary());
+
+        assertEquals(expectedData.getData().getprofile_image(),
+                actualData.getData().getprofile_image());
+
+        assertEquals(expectedData.getMessage(), actualData.getMessage());
+
+//----------------------------------------------------
+        //Serializaztion java yapısında olan dataları json formatına dönüştürme işlemidir.
+        //Gson sınıfından bir obje üretilir
+
+        Gson gson = new Gson();
+        String jsonFromJava = gson.toJson(actualData);
+        System.out.println(jsonFromJava);
 
 
-
-
-
-
-    }}
+    }
+}
